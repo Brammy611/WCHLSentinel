d0a3190a958f11bf04db98702c3b1feaee47fdc2
Bramantyo Kunni Nurrisqi <bramantyo989@gmail.com>
2025-08-26 23:34:00 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+// App.js
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import ExamRoom from './pages/ExamRoom';
 import Result from './pages/Result';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
+import PreExam from './components/PreExam';
 import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
@@ -14,16 +16,29 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
+          
           <Route path="/dashboard" element={
             <ProtectedRoute>
               <Dashboard />
             </ProtectedRoute>
           } />
+
+          <Route
+            path="/preExam/:id"
+            element={
+              <ProtectedRoute>
+                <PreExam />
+              </ProtectedRoute>
+            }
+          />
+
+
           <Route path="/exam/:id" element={
             <ProtectedRoute>
               <ExamRoom />
             </ProtectedRoute>
           } />
+          
           <Route path="/result/:id" element={
             <ProtectedRoute>
               <Result />
